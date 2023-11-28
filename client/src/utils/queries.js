a3a19219086e0183b98d2670fdd8a0a9050beb98
Cervp22 <135301185+Cervp22@users.noreply.github.com>
2023-11-27 20:08:51 -0800

    Update queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,3 +1,15 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_REVIEWS = gql`
+  query getReviews($content: String!, $user_id: ID!) {
+    reviews(content: $content, user_id: $user_id) {
+      _id
+      content
+      user_id
+    }
+  }
+`;
+
 export const QUERY_PRODUCTS = gql`
   query getProducts($category: ID) {
     products(category: $category) {
