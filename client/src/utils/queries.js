fa42d05bdb617b9542d0d601697054d1f7cc49ea
Paul Cervantes <paulcervantes2244@gmail.com>
2023-11-27 19:12:18 -0800

    editing page communication to datbase

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,11 +1,14 @@
-import { gql } from '@apollo/client';
-
+import { gql } from "@apollo/client";
 
 export const QUERY_REVIEWS = gql`
-  reviews()
-`
-
-
+  query getReviews($content: String!, $user_id: ID!) {
+    reviews(content: $content, user_id: $user_id) {
+      _id
+      content
+      user_id
+    }
+  }
+`;
 export const QUERY_PRODUCTS = gql`
   query getProducts($category: ID) {
     products(category: $category) {
